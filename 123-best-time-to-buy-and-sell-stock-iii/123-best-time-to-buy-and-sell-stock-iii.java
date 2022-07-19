4e8436fe535d9ef75634d621919ce23d4ca69230
Naveen Kumar <35249536+Naveen1023@users.noreply.github.com>
2022-07-19 19:29:17 +0530

    Time: 203 ms (14.41%), Space: 124.8 MB (17.10%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.java
@@ -16,28 +16,48 @@ class Solution {
     
     public int solveTabulation(int arr[]){
         int n = arr.length;
-        int t[][][] = new int[n+1][2][3];
+//         int t[][][] = new int[n+1][2][3];
         
-        t[n][0][0] = 0;t[n][0][1] = 0;t[n][0][2] = 0;
-        t[n][1][0] = 0;t[n][1][1] = 0;t[n][1][2] = 0;
+//         t[n][0][0] = 0;t[n][0][1] = 0;t[n][0][2] = 0;
+//         t[n][1][0] = 0;t[n][1][1] = 0;t[n][1][2] = 0;
         
+        
+//         for(int i = n-1;i>=0;i--){
+//             for(int buy = 0;buy<=1;buy++){
+//                 for(int maxTran = 1;maxTran < 3;maxTran++){
+//                     if(buy > 0){             //max same as tran. is not completed yet
+//                         t[i][buy][maxTran] = Math.max(-arr[i] + t[i+1][0][maxTran] , t[i+1][1][maxTran]);
+//                     }
+//                     else{
+//                         t[i][buy][maxTran] =  Math.max(arr[i] + t[i+1][1][maxTran-1] , t[i+1][0][maxTran]);
+//                     }
+//                 }
+                
+//             }
+//         }
+        
+//         return t[0][1][2];
+        
+        
+        // SPACE Optimized 1 
+        
+        int after[][] = new int[2][3];
+        int curr[][] = new int[2][3];
         
         for(int i = n-1;i>=0;i--){
             for(int buy = 0;buy<=1;buy++){
                 for(int maxTran = 1;maxTran < 3;maxTran++){
-                    if(buy > 0){             //max same as tran. is not completed yet
-                        t[i][buy][maxTran] = Math.max(-arr[i] + t[i+1][0][maxTran] , t[i+1][1][maxTran]);
-                    }
-                    else{
-                        t[i][buy][maxTran] =  Math.max(arr[i] + t[i+1][1][maxTran-1] , t[i+1][0][maxTran]);
-                    }
+                    
+                    if(buy > 0)
+                        curr[buy][maxTran] =  Math.max(-arr[i] + after[0][maxTran], after[1][maxTran]);
+                    else
+                        curr[buy][maxTran] =  Math.max(arr[i] + after[1][maxTran-1], after[0][maxTran]);
                 }
-                
             }
+            after = curr;
         }
-        
-        return t[0][1][2];
-        
+        return after[1][2];
+                
     }
     
     public int solve(int arr[],int i, int buy, int maxTran, int t[][][]){
