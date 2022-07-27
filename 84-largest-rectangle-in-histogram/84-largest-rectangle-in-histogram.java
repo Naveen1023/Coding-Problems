3fd7cc71555e9499da90817e709a5cd6ef508f7d
Naveen Kumar <35249536+Naveen1023@users.noreply.github.com>
2022-07-27 17:53:35 +0530

    Time: 417 ms (6.93%), Space: 96.3 MB (25.16%) - LeetHub

---
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        
+       // rectangle at one index can expand to left and right side till increasing histograms are coming.. so we need to find the very first left side smaller and right side smaller element index as till that point histogram will expland...
+        
+        int leftSmaller[] = new int[heights.length];
+        int rightSmaller[] = new int[heights.length];
+        
+        Stack<Integer> st = new Stack<>();
+        
+        for(int i=0;i<heights.length;i++){
+            while(!st.isEmpty() && heights[st.peek()] >= heights[i]) st.pop();
+            
+            if(st.isEmpty()) leftSmaller[i] = -1;
+            else leftSmaller[i] = st.peek();
+            st.push(i);
+            
+        }
+        
+        // System.out.println(Arrays.toString(leftSmaller));
+        
+        st.clear();
+        for(int i=heights.length - 1;i>=0;i--){
+            while(!st.isEmpty() && heights[st.peek()] >= heights[i]) st.pop();
+            
+            if(st.isEmpty()) rightSmaller[i] = heights.length;
+            else rightSmaller[i] = st.peek();
+            st.push(i);
+        } 
+        // 0   1   2   3   4   5
+        // 2   1   5   6   2   3
+// ls =   -1  -1   1   2   1   4
+// rs =    1   6   4   4   6   6
+        
+        // System.out.println(Arrays.toString(rightSmaller));
+        int res = 0;
+        for(int i=0;i<heights.length;i++){
+            res = Math.max(res, (rightSmaller[i] - leftSmaller[i] - 1 )* heights[i]);
+        }
+        
+        return res;
+    }
+}
