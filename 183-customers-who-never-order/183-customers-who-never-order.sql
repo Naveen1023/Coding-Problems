c22112b61056344f5aa8950b282ea020c1a7dec5
Naveen Kumar <35249536+Naveen1023@users.noreply.github.com>
2022-08-01 09:21:16 +0530

    Time: 577 ms (50.17%), Space: 0B (100.00%) - LeetHub

--- 183-customers-who-never-order/183-customers-who-never-order.sql
+++ 183-customers-who-never-order/183-customers-who-never-order.sql
@@ -1,3 +1,9 @@
 # Write your MySQL query statement below
-select name as Customers from Customers
-Where id NOT In(select customerId from Orders);
+
+
+# # select name as Customers from Customers
+# Where id NOT In(select customerId from Orders);
+
+
+select C.name AS Customers from Customers C
+where NOT EXISTS(Select O.customerId from Orders O where C.id = O.customerId);
