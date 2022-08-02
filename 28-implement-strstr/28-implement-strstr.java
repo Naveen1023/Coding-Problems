1803bd21bba19b6775be4db0da3a9db7df1c368e
Naveen Kumar <35249536+Naveen1023@users.noreply.github.com>
2022-08-02 17:18:26 +0530

    Time: 1 ms (65.35%), Space: 42.5 MB (15.58%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.java
+++ 28-implement-strstr/28-implement-strstr.java
@@ -2,18 +2,67 @@ class Solution {
     public int strStr(String haystack, String needle) {
         int m = haystack.length();
         int n = needle.length();
-        int ans = -1;
-        for(int i=0;i<m-n+1;i++){
-            
-            for(int j=0;j<n;j++){
-                if(needle.charAt(j) != haystack.charAt(i+j)) break;
-                else if(j == n-1) {
-                    ans = i;
-                }
-            }
-            if(ans != -1) return ans;
-        }
-        return ans;
         
+        // ...................Plain Serching  O(m*n) Solution ............................
+        
+//         int ans = -1;
+//         for(int i=0;i<m-n+1;i++){
+            
+//             for(int j=0;j<n;j++){
+//                 if(needle.charAt(j) != haystack.charAt(i+j)) break;
+//                 else if(j == n-1) {
+//                     ans = i;
+//                 }
+//             }
+//             if(ans != -1) return ans;
+//         }
+//         return ans;
+        
+        
+        // ........................KMP ALgorithm  O(n + m) Solution ............................
+        
+        /*
+        1. compute the LPS array... Longest Prefix Suffix. This will help up to reiterate over the already matched string...
+        
+        
+        
+        
+        */
+        int lps[] = computeLPS(needle);
+        int i=0,j=0;
+        while(i < haystack.length()){
+            if(haystack.charAt(i) == needle.charAt(j)){
+                i++;j++;
+            }
+            else{
+                if(j == 0)
+                    i++;
+                else
+                    j = lps[j-1];
+            }
+            if(j == needle.length()) return i - needle.length();
+        }
+        return -1;
+    }
+    // AAAX => [0,1,2,0]
+    public int[] computeLPS(String s){   // O(s.length()) 
+        int n = s.length();
+        int lps[] = new int[n];
+        
+        int i = 1, prevLPS = 0;
+        while(i < s.length()){
+            if(s.charAt(i) == s.charAt(prevLPS)){
+                lps[i] = prevLPS+1;
+                prevLPS++;
+                i++;
+            }
+            else{
+                if(prevLPS == 0)
+                    lps[i++] = 0;
+                else 
+                    prevLPS = lps[prevLPS-1];
+            }
+        } 
+        return lps;        
     }
 }
