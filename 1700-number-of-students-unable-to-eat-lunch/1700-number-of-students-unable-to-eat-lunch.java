34a79669d97861d7af67fa2312e5e8d7a580fc8e
Naveen Kumar <35249536+Naveen1023@users.noreply.github.com>
2022-08-15 12:37:20 +0530

    Time: 0 ms (100.00%), Space: 42.7 MB (14.31%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.java
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.java
@@ -1,25 +1,49 @@
 class Solution {
     public int countStudents(int[] stu, int[] sand) {
         
-        Queue<Integer> q = new LinkedList<>();
+        // top sandwich is going to be consumed by the student any where in the queue if he likes it...
+        int one =0;
+        int zero = 0;
         
-        int index = 0;
-        
-        for(int ele : stu)
-            q.offer(ele);
-        
-        while(!q.isEmpty()){
-            int size = q.size();
-            int count = 0;
-            while(!q.isEmpty() && q.peek() != sand[index]){
-                count++;
-                int temp = q.poll();
-                q.add(temp);
-                if(count > size) return size; // we rorated once still not equals to the top sandwich
-            }
-            q.poll();
-            index++;
+        for(int ele : stu){
+            if(ele == 1) one++;
+            else zero++;
         }
-        return 0;        
-    }
+        
+        for(int s : sand){
+            if(s==1){
+                if(one > 0) one--;
+                else return zero;
+            }
+            else{
+                if(zero > 0) zero--;
+                else return one;
+            }   
+            
+        }
+        
+        return 0;
+        
+    } 
+//         Queue<Integer> q = new LinkedList<>();
+        
+//         int index = 0;
+        
+//         for(int ele : stu)
+//             q.offer(ele);
+        
+//         while(!q.isEmpty()){
+//             int size = q.size();
+//             int count = 0;
+//             while(!q.isEmpty() && q.peek() != sand[index]){
+//                 count++;
+//                 int temp = q.poll();
+//                 q.add(temp);
+//                 if(count > size) return size; // we rorated once still not equals to the top sandwich
+//             }
+//             q.poll();
+//             index++;
+//         }
+//         return 0;        
+//     }
 }
