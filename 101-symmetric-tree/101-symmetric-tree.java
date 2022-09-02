5db015f2edb37dd60968bb0a60f5165100d65b91
Naveen Kumar <35249536+Naveen1023@users.noreply.github.com>
2022-09-02 22:43:07 +0530

    Time: 1 ms (63.63%), Space: 42.8 MB (19.56%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.java
+++ 101-symmetric-tree/101-symmetric-tree.java
@@ -18,10 +18,13 @@ class Solution {
         return solve(root,root);
     }
     public boolean solve(TreeNode n1 , TreeNode n2){
+        
         if(n1 == null && n2 == null) return true;
         
-        if((n1 == null && n2 != null) || (n1 != null && n2 == null) || (n1.val != n2.val) ) return false;
-        
+        if((n1 == null && n2 != null) || (n1 != null && n2 == null) || (n1.val != n2.val)) return false;
+         
         return solve(n1.left, n2.right) && solve(n1.right, n2.left);
+        
+        
     }
 }
