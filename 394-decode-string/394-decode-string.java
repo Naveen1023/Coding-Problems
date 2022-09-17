2b5ad646e25da689035c730169dd2c4d042497f8
Naveen Kumar <35249536+Naveen1023@users.noreply.github.com>
2022-09-18 00:23:25 +0530

    Time: 2 ms (61.10%), Space: 41.6 MB (80.13%) - LeetHub

--- 394-decode-string/394-decode-string.java
+++ 394-decode-string/394-decode-string.java
@@ -1,46 +1,40 @@
 class Solution {
     public String decodeString(String s) {
-        Stack<String> st= new Stack<>();
-        Stack<Integer> counter = new Stack<>();
-        int n = s.length();
-        int i = 0;
         
-        String res = "";
-        while(i < s.length()){
-            
-            char ch = s.charAt(i);
+        Queue<Character> q = new LinkedList<>();
+        for(char ch : s.toCharArray()){
+            q.offer(ch);
+        }
+        return solve(q);
+    }
+    
+    public String solve(Queue<Character> q){
+        
+        StringBuilder temp = new StringBuilder();
+        
+        int num = 0;
+        while(!q.isEmpty()){
+            char ch = q.poll();
             
             if(Character.isDigit(ch)){
-                int val = 0;
-                while(i < n && Character.isDigit(s.charAt(i))){
-                    val = val * 10 + (s.charAt(i)-'0');
-                    i++;
-                }
-                counter.push(val);
+                num = num * 10 + ch-'0';
             }
-            else if(ch == '['){
-                st.push(res);
-                res = "";
-                i++;
+            else if (ch == '[') {
+                String sub = solve(q);
+                for (int i = 0; i < num; i++) temp.append(sub);   
+                num = 0;
             }
             else if(ch == ']'){
-                String rep = st.pop();
-                int times = counter.pop();
-                while(times-- > 0){
-                    rep += res;
-                }
-                res = rep;
-                i++;
+                break;
             }
-            else{ // means this is jsut a character
-                res += ch;
-                i++;
+            else{ //means a character
+                temp.append(ch);
             }
-        }        
+        }
+        return temp.toString();
         
-        
-        return res;
     }
+    
 }
 
 /*
