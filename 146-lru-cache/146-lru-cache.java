4e3fdcfcf8f6f937984af32d72d5abce806003ea
Naveen Kumar <35249536+Naveen1023@users.noreply.github.com>
2022-09-20 00:38:49 +0530

    Time: 145 ms (10.08%), Space: 167.8 MB (14.56%) - LeetHub

--- 146-lru-cache/146-lru-cache.java
+++ 146-lru-cache/146-lru-cache.java
@@ -6,17 +6,16 @@ class Node{
     Node(int k, int v){
         this.key = k;
         this.val = v;
-    }    
+    }
 }
 
 class LRUCache {
     int cap;
     HashMap<Integer, Node> map;
-    Node head;
-    Node tail;
+    Node head, tail;
     public LRUCache(int capacity) {
+        this.cap = capacity;
         map = new HashMap<>();
-        cap = capacity;
         head = new Node(-1,-1);
         tail = new Node(-1,-1);
         head.next = tail;
@@ -32,34 +31,40 @@ class LRUCache {
     }
     
     public void put(int key, int value) {
-        if(map.containsKey(key)){ // just update the value...
+        // see if the key is repeated or not
+        // repeated => update the value
+        // if not => insert from scratch...
+        
+        if(map.containsKey(key)){
             Node node = map.get(key);
-            node.val = value; // update the priority as it is just now got used...
+            node.val = value;
             deleteNode(node);
             addToHead(node);
         }
-        else{ // we need to insert new node, and update it;s priority...
+        else{
+            // insert node at head location as it is most recent
             Node node = new Node(key, value);
-            map.put(key, node);
+            map.put(key,node);
             addToHead(node);
+            
             if(map.size() > cap){
+                //then remvoe the least used node which is at TAIL
                 map.remove(tail.prev.key);
-                deleteNode(tail.prev);
+                deleteNode(tail.prev);  
+                
             }
+            
         }
     }
-     // head ==> 1  
-    public void addToHead(Node node){
-        node.next = head.next;
-        node.next.prev = node;
-        node.prev= head;
-        head.next = node;
-        
-    }
-    // 1==>2==>3
     public void deleteNode(Node node){
         node.prev.next = node.next;
         node.next.prev = node.prev;
+    }
+    public void addToHead(Node node){
+        node.next = head.next;
+        head.next.prev = node;
+        node.prev= head;
+        head.next = node;
     }
     
 }
