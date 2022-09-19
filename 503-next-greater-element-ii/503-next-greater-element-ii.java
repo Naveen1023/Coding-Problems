be3720d54b65ade705417768ff321245bcfa2b9c
Naveen Kumar <35249536+Naveen1023@users.noreply.github.com>
2022-09-19 21:51:03 +0530

    Time: 48 ms (34.58%), Space: 55 MB (46.05%) - LeetHub

--- 503-next-greater-element-ii/503-next-greater-element-ii.java
+++ 503-next-greater-element-ii/503-next-greater-element-ii.java
@@ -3,24 +3,49 @@ class Solution {
         // circular means {1,5,3,4,3} angain {1,5,3,4,3}.. 
         //                        (i) ans will be 5 
         
-        Stack<Integer> st= new Stack<Integer>();
-        for(int i=arr.length-1;i>=0;i--){
-            while(!st.isEmpty() && st.peek() <= arr[i]) st.pop();
-            if(st.isEmpty()){} // do nothing...
-            else{} // do nothing...
-            st.push(arr[i]);    
-        }
+//         Stack<Integer> st= new Stack<Integer>();
+//         for(int i=arr.length-1;i>=0;i--){
+//             while(!st.isEmpty() && st.peek() <= arr[i]) st.pop();
+//             if(st.isEmpty()){} // do nothing...
+//             else{} // do nothing...
+//             st.push(arr[i]);    
+//         }
        
-        // System.out.println(st);
-        for(int i=arr.length-1;i>=0;i--){
-            int curr = arr[i];
+//         // System.out.println(st);
+//         for(int i=arr.length-1;i>=0;i--){
+//             int curr = arr[i];
+//             while(!st.isEmpty() && st.peek() <= arr[i]) st.pop();
+            
+//             if(st.isEmpty()) arr[i] = -1;
+//             else arr[i] = st.peek();
+            
+//             st.push(curr);    
+//         }
+//         return arr;
+        int n = arr.length, index = 0;
+        int res[] = new int[n];
+        Stack<Integer> st=  new Stack<>();
+//          3 2 4
+//          3 
+        for(int i=n-1;i>=0;i--){
             while(!st.isEmpty() && st.peek() <= arr[i]) st.pop();
             
-            if(st.isEmpty()) arr[i] = -1;
-            else arr[i] = st.peek();
+            // res[index++] = st.peek();
+            st.push(arr[i]);
             
-            st.push(curr);    
         }
-        return arr;
+        
+        // st = new Stack<>();
+        for(int i=n-1;i>=0;i--){
+            while(!st.isEmpty() && st.peek() <= arr[i]) st.pop();
+            if(st.isEmpty()) {
+                res[i] = -1;
+            }
+            else res[i] = st.peek();
+            st.push(arr[i]);
+        }
+        
+        return res;
+        
     }
 }
