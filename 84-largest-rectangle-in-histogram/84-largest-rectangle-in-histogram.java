6b3974f783c16570062b5839b8a0f79bba6df15a
Naveen Kumar <35249536+Naveen1023@users.noreply.github.com>
2022-09-20 15:37:59 +0530

    Time: 386 ms (9.65%), Space: 95.9 MB (27.78%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
@@ -1,40 +1,37 @@
 class Solution {
-    public int largestRectangleArea(int[] heights) {
+    public int largestRectangleArea(int[] arr) {
         
-       // rectangle at one index can expand to left and right side till increasing histograms are coming.. so we need to find the very first left side smaller and right side smaller element index as till that point histogram will expland...
-        int res = 0;
-        int leftSmaller[] = new int[heights.length];
-        int rightSmaller[] = new int[heights.length];
+        int res = arr[0];
+        // we need to find previous smaller element and next smaller element...
         
+        int ps[] = new int[arr.length];
+        int ns[] = new int[arr.length];
         Stack<Integer> st = new Stack<>();
         
-        for(int i=0;i<heights.length;i++){
-            while(!st.isEmpty() && heights[st.peek()] >= heights[i]) st.pop();
-            
-            if(st.isEmpty()) leftSmaller[i] = -1;
-            else leftSmaller[i] = st.peek();
+        for(int i=0;i<arr.length;i++){
+            while(!st.isEmpty() && arr[st.peek()] >= arr[i]) st.pop();
+            if(st.isEmpty()){
+                ps[i] = -1;
+            }
+            else ps[i] = st.peek();
             st.push(i);
-            
-        }
-        
-        // System.out.println(Arrays.toString(leftSmaller));
-        
+        }       
         st.clear();
-        for(int i=heights.length - 1;i>=0;i--){
-            while(!st.isEmpty() && heights[st.peek()] >= heights[i]) st.pop();
+        
+        for(int i=arr.length-1;i>=0;i--){
+            while(!st.isEmpty() && arr[st.peek()] >= arr[i]) st.pop();
+            if(st.isEmpty()) ns[i] = arr.length;
+            else ns[i] = st.peek();
             
-            if(st.isEmpty()) rightSmaller[i] = heights.length;
-            else rightSmaller[i] = st.peek();
             st.push(i);
-            res = Math.max(res, (rightSmaller[i] - leftSmaller[i] - 1 )* heights[i]);
-        } 
-        // 0   1   2   3   4   5
-        // 2   1   5   6   2   3
-// ls =   -1  -1   1   2   1   4
-// rs =    1   6   4   4   6   6
+        }
+        // System.out.println(Arrays.toString(ps));
+        // System.out.println(Arrays.toString(ns));
         
-        // System.out.println(Arrays.toString(rightSmaller));
-        
+        for(int i=0;i<arr.length;i++){
+            res = Math.max(res, (ns[i]-ps[i]-1)*arr[i]);
+        }
+                
         return res;
     }
 }
