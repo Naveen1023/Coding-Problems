249a69b31cc9206356f0c0b05dea756b18959c96
Naveen Kumar <35249536+Naveen1023@users.noreply.github.com>
2022-09-22 18:48:31 +0530

    Time: 105 ms (11.56%), Space: 69 MB (8.34%) - LeetHub

---
+++ 322-coin-change/322-coin-change.java
@@ -0,0 +1,29 @@
+class Solution {
+    Integer t[][] ;
+    public int coinChange(int[] coins, int target) {
+        
+        t = new Integer[coins.length][target+1];
+        int ans = solve(coins.length-1,coins, target) ;
+        
+        return (ans == (int)(1e8)) ?  -1 : ans;
+                   
+    }
+    
+    public int solve(int i, int arr[] , int target){
+        // System.out.println(i+" "+target);
+        
+        if(target == 0) return 0;
+       
+        if(i < 0) return (int)1e8;
+        
+        if(t[i][target] != null) return t[i][target];
+        
+        if(arr[i] <= target){
+            return t[i][target] = Math.min(1+solve(i,arr,target-arr[i]) , solve(i-1,arr,target));
+        }
+        else
+            return t[i][target] = solve(i-1,arr,target);
+        
+    }
+    
+}
