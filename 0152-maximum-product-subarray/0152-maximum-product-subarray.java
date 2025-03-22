bc3ac18d1ae689628ea4312995633352ac55eb72
Naveen Kumar <35249536+Naveen1023@users.noreply.github.com>
2025-03-23 02:21:45 +0530

    Time: 0 ms (100%), Space: 45.2 MB (30.8%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int maxProduct(int[] arr) {
+        int res = Integer.MIN_VALUE;
+
+        int prod = 1;
+        for (int i = 0; i < arr.length; i++) {
+            prod *= arr[i];
+            res = Math.max(res, prod);
+
+            if (prod == 0) {
+                prod = 1;
+            }
+        }
+        prod = 1;
+        for (int i = arr.length - 1; i >= 0; i--) {
+            prod *= arr[i];
+            res = Math.max(res, prod);
+
+            if (prod == 0) {
+                prod = 1;
+            }
+        }
+        return res;
+    }
+}
+
+/**
+ * 1. Brute force of finding all subsets O(n^2)
+ * 2. Kadane Algo. => max(left to right, right to left)
+ * 
+ */
