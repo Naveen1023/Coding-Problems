34a5c83fdeb9b1e645f6ca389419abda75fb2530
Naveen Kumar <35249536+Naveen1023@users.noreply.github.com>
2025-03-23 02:24:49 +0530

    Time: 0 ms (100%), Space: 44.8 MB (85.74%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -26,6 +26,7 @@ class Solution {
 
 /**
  * 1. Brute force of finding all subsets O(n^2)
- * 2. Kadane Algo. => max(left to right, right to left)
+ * 2. Kadane Algo. => max(left to right, right to left). 
+reasoning : we just need to exclude one -ve number if there are odd -ve numbers. For the rest we can simply multiply. Handle 0 by resetting the product.
  * 
  */
